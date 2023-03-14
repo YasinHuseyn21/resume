98165293b073e371c6b64b8985295f845244d6ed
YasinHuseyn <forego687@gmail.com>
2023-03-15 03:14:06 +0400

    "Yeap commit"

--- ResumeAppByUsingSpring/ResumeDbAppJpaSpring/ResumeDbAppJpaSpring/src/main/java/com/company/dao/impl/UserDaoImpl.java
+++ ResumeAppByUsingSpring/ResumeDbAppJpaSpring/ResumeDbAppJpaSpring/src/main/java/com/company/dao/impl/UserDaoImpl.java
@@ -4,81 +4,66 @@ import com.company.dao.inter.AbstractDao;
 import com.company.dao.inter.UserDaoInter;
 import com.company.entity.User;
 
+
+
+import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
 
+@Repository
 public class UserDaoImpl extends AbstractDao implements UserDaoInter {
-    
-    
+   @PersistenceContext
+    EntityManager em;
+
+
     //NamedQuery      
-    public User findByEmail(String email){
-    EntityManager em=em();
-    Query query=em.createNamedQuery("User.findByEmail", User.class);
-    query.setParameter("email",email);
-    List<User> list=query.getResultList();
-    if(list.size()==1){
-    return list.get(0);
+    public User findByEmail(String email) {
+        Query query = em.createNamedQuery("User.findByEmail", User.class);
+        query.setParameter("email", email);
+        List<User> list = query.getResultList();
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+        return null;
     }
-    return  null;
-    }
-    
-    
-      @Override
+
+
+    @Override
     public List<User> getAll() {
-       EntityManager em=em();
-       Query query =em.createQuery("select u from User u",User.class);
-       
-        List<User> list= query.getResultList();
+        Query query = em.createQuery("select u from User u", User.class);
+        List<User> list = query.getResultList();
         return list;
-        
+
     }
-    
+
     @Override
     public User getById(int userId) {
-         EntityManager entitymanager = em();
-      User u=entitymanager.find(User.class, userId);
-      entitymanager.close();
-             return u;
-       
+        User u = em.find(User.class, userId);
+        return u;
+
     }
 
     @Override
     public boolean updateUser(User user) {
-        
-      EntityManager entitymanager = em();
-      entitymanager.getTransaction().begin();
-      entitymanager.merge(user);
-      entitymanager.getTransaction().commit();
-      entitymanager.close();
-            return true;
+        em.merge(user);
+        return true;
     }
 
     @Override
     public boolean removeUser(int id) {
-      
-      EntityManager entitymanager = em();
-       User u=entitymanager.find(User.class,id);
-       entitymanager.getTransaction().begin();
+        User u = em.find(User.class, id);
+        em.remove(u);
+        return true;
 
-      entitymanager.remove(u);
-      entitymanager.getTransaction().commit();
-      entitymanager.close();
-      return true;
-      
     }
 
     @Override
     public boolean addUser(User user) {
-      EntityManager entitymanager = em();
-      entitymanager.getTransaction().begin();
-      entitymanager.persist(user);
-      entitymanager.getTransaction().commit();
-      entitymanager.close();
-      return true;
+        em.persist(user);
+        return true;
     }
-
-  
 
 
 }
