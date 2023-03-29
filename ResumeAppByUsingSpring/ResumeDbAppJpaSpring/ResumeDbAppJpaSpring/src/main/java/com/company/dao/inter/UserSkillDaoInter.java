c4b8abce993228f133c76dda7c93e42b6e3cf3f7
YasinHuseyn <forego687@gmail.com>
2023-03-30 02:34:30 +0400

    ResumeProject

--- ResumeAppByUsingSpring/ResumeDbAppJpaSpring/ResumeDbAppJpaSpring/src/main/java/com/company/dao/inter/UserSkillDaoInter.java
+++
@@ -1,9 +0,0 @@
-package com.company.dao.inter;
-
-import com.company.entity.UserSkill;
-
-import java.util.List;
-
-public interface UserSkillDaoInter {
-    public List<UserSkill> getAllSkillByUserId(Integer userId);
-}
